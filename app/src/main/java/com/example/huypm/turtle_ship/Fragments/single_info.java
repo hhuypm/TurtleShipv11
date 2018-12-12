b3a645d5a3add857e9f725353c9b8d5813b16d7d
Tung <win17061997@gmail.com>
2018-12-12 22:09:59 +0700

    create single infomation

---
+++ app/src/main/java/com/example/huypm/turtle_ship/Fragments/single_info.java
@@ -0,0 +1,32 @@
+package com.example.huypm.turtle_ship.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.huypm.turtle_ship.R;
+
+public class single_info extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.single_infomation,container,false);
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Thông tin đơn hàng");
+    }
+}
