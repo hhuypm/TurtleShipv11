87cb6bac8d3b2e2ed60f01e33e96ab9a030aa189
luffysan1997 <0975192529>
2018-12-12 22:05:39 +0700

    xử lí 2 nút với icon

--- app/src/main/java/com/example/huypm/turtle_ship/Fragments/status_oder.java
+++ app/src/main/java/com/example/huypm/turtle_ship/Fragments/status_oder.java
@@ -1,5 +1,6 @@
 package com.example.huypm.turtle_ship.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,19 +8,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.huypm.turtle_ship.R;
 
 public class status_oder extends Fragment {
+    private  Button btn_daship;
+    private  Button btn_nhanship;
+    private  Boolean statusbt1=true;
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Quản lý đơn hàng");
+
+        btn_nhanship = view.findViewById(R.id.btn_nhanship);
+        btn_daship = view.findViewById(R.id.btn_daship);
+
+
+        btn_nhanship.setBackgroundColor(getResources().getColor(R.color.green));
+
+        btn_nhanship.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(statusbt1==false) {
+                    btn_nhanship.setBackgroundColor(getResources().getColor(R.color.green));
+                    btn_daship.setBackgroundColor(getResources().getColor(R.color.white));
+                }
+                statusbt1=true;
+            }
+        });
+
+        btn_daship.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(statusbt1==true) {
+                    btn_nhanship.setBackgroundColor(getResources().getColor(R.color.white));
+                    btn_daship.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+                statusbt1=false;
+            }
+        });
+
     }
 
     @Nullable
