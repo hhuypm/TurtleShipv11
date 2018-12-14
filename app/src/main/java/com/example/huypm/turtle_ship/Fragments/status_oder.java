d5ff119fbe4d7035b6071c08d9d634ef47b8440b
hhuypm <pmh13797@gmail.com>
2018-12-14 21:51:24 +0700

    - update 9h51

--- app/src/main/java/com/example/huypm/turtle_ship/Fragments/status_oder.java
+++ app/src/main/java/com/example/huypm/turtle_ship/Fragments/status_oder.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +32,11 @@ public class status_oder extends Fragment {
 
         btn_nhanship = view.findViewById(R.id.btn_nhanship);
         btn_daship = view.findViewById(R.id.btn_daship);
+        Bundle bundle = getArguments();
+
+        //Đây iD shipper
+        String IdShipper = String.valueOf(bundle.getInt("ID"));
+
 
 
         btn_nhanship.setBackgroundColor(getResources().getColor(R.color.green));
